faaab1dbcfd9d35f1e06b8d971f7ca14c8ed5910
vijayved690-ux <vijayved690@gmail.com>
2026-04-27 19:26:46 +0530

    Update Coupon.js

--- models/Coupon.js
+++ models/Coupon.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 
 const couponSchema = new mongoose.Schema({
-    code: { type: String, required: true, unique: true, length: 5 },
+    code: { type: String, required: true, unique: true },
     discountPercentage: { type: Number, required: true },
-    targetName: { type: String }, 
-    doctorPhone: { type: String, required: true }, 
-    location: { type: String }, 
-    proPhone: { type: String }, 
-    audienceType: { type: String, enum: ['Doctor', 'Patient'], required: true },
-    isUsed: { type: Boolean, default: false },
-    redeemedAt: { type: Date }, 
-    source: { type: String, enum: ['Campaign', 'Requested'], default: 'Campaign' },
+    targetName: { type: String },
+    doctorPhone: { type: String },
+    proPhone: { type: String },
+    location: { type: String },
+    audienceType: { type: String },
+    source: { type: String },
     expiryDate: { type: Date, required: true },
-    createdAt: { type: Date, default: Date.now }
-});
+    isUsed: { type: Boolean, default: false },
+    redeemedAt: { type: Date },
+    branchRedeemed: { type: String } // Yeh naya add hua hai
+}, { timestamps: true });
 
-couponSchema.index({ doctorPhone: 1, createdAt: 1 });
 module.exports = mongoose.model('Coupon', couponSchema);
