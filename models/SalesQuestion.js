9068509832e219eae24bdcafc6586ed8752e4d63
vijayved690-ux <vijayved690@gmail.com>
2026-05-08 19:51:43 +0530

    Update SalesQuestion.js

--- models/SalesQuestion.js
+++ models/SalesQuestion.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-    keyword: { type: String, required: true, unique: true }, // e.g., SS46, B1
-    questions: [{ type: String }] // Array of 3-4 questions
+    keyword: { type: String, required: true, unique: true }, 
+    team: { type: String, required: true }, // 'SS' or 'B'
+    time: { type: String, required: true }, // Format: "09:00", "14:30"
+    questions: [{ type: String }] // Dynamic array (Jitne chahe utne questions)
 }, { timestamps: true });
 
 module.exports = mongoose.model('SalesQuestion', schema);
